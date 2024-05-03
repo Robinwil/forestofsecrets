54d5f5787b8fa0324617f48335ff00237f1e9414
robinwil <robin.wilhelmsen@me.com>
2024-05-04 01:44:23 +0200

    -New things

--- vue.config.js
+++
@@ -1,8 +0,0 @@
-const { defineConfig } = require("@vue/cli-service");
-
-module.exports = defineConfig({
-  transpileDependencies: true,
-  outputDir: "dist",
-  assetsDir: "static",
-  indexPath: "index.html",
-});
